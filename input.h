86a2c703e0609967dfa507e917bdb32ad4452038
jesuslovesyoutoday <jesuslovesyoutoday@yadex.ru>
2021-09-20 23:01:57 +0300

    docs for enum

--- input.h
+++ input.h
@@ -3,26 +3,33 @@
 
 #include <string.h>
 
-//--------------------------------------
+//---------------------------------------
 //!
-//! Asks user to enter files' names to
-//! read from and write to and checks 
-//! either they are available or not
+//! Enum to log errors while opening file
 //!
-//! @param[out] <input_file> pointer to
-//!                          the name of
-//!                          input file
-//! @param[out] <buffer> pointer to the
-//!                      allocated memory
-//!                      to write input
-//!
-//!-------------------------------------
+//---------------------------------------
 
 enum InputStatus {
     OK = 0,
     DOESNT_EXIST = 1,
     EMPTY_FILE = 2
 };
+
+//---------------------------------------
+//!
+//! Asks user to enter files' names to
+//! read from and write to and checks 
+//! either they are available or not
+//!
+//! @param[out] <input_file> name of
+//!                          input file
+//! @param[out] <output_file> name of
+//!                          output file
+//! @param[out] <buffer> pointer to the
+//!                      allocated memory
+//!                      to write input
+//!
+//!--------------------------------------
     
 enum InputStatus input(char* input_file, char* output_file, char** buffer);
 
